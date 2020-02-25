8c7a9a8fd1b80b4087c854ff457862df2dfeb6ef
Wallas Faria <wallasfaria@hotmail.com>
2020-02-25 17:37:47 -0300

    configura idioma adiciona gems

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.default_locale = 'pt-BR'
+    config.i18n.available_locales = 'pt-BR'
+    config.time_zone = "Brasilia"
+    config.active_record.default_timezone = :local
   end
 end
