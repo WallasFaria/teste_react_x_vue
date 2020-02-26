921a52e05d9d880e0c322bf7aae96ae3551dea38
Wallas Faria <wallasfaria@hotmail.com>
2020-02-26 12:38:36 -0300

    adiciona exemplo

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Produto } from './produto.model';
 
 function App() {
-  const p: Produto = { id: 23, nome: 'Produto teste' }
+  const [produtos, setProdutos] = useState<Array<Produto>>([
+    { id: 23, nome: 'Produto teste' }
+  ])
+
+  const [novoProduto, setNovoProduto] = useState('')
+
+  function add(): void {
+    setProdutos([...produtos, { id: produtos.length + 1, nome: novoProduto }])
+    setNovoProduto('')
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          <br />
-          {p.nome}, id: {p.id}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          <input type="text"
+            value={novoProduto}
+            onChange={e => setNovoProduto(e.target.value)}
+            onKeyPress={e => e.key === 'Enter' && add()} />
+          <button onClick={add}>Add</button>
+        </div>
+
+        <ul>
+          {produtos.map(produto => (
+            <li key={produto.id}>{produto.nome}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
